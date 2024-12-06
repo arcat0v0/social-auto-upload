cc7b096f33dbdd0b4ccb020f536108909be9c1c5
arcat0v0 <i@xvx.rs>
2024-12-06 22:08:35 +0800

    :bug: change headless to true

--- app.py
+++ app.py
@@ -32,8 +32,8 @@ browser_instances = {
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     playwright = await async_playwright().start()
-    browser_instances["firefox"] = await playwright.firefox.launch(headless=False)
-    browser_instances["chromium"] = await playwright.chromium.launch(headless=False)
+    browser_instances["firefox"] = await playwright.firefox.launch(headless=True)
+    browser_instances["chromium"] = await playwright.chromium.launch(headless=True)
 
     yield
     await browser_instances["firefox"].close()
